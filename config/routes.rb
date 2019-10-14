c836f6400957675801f29c635ee9e6248747416d
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2019-10-14 22:32:08 +0300

    Root to message templates index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "message_templates#index"
   resources :messages
   resources :message_templates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
